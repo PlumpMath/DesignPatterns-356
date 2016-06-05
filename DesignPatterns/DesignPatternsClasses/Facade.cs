0b0bc917d1ac2b1e38a579810e2fd70e46dcd1c4
SYaytsev <yaytsevs@gmail.com>
2016-06-06 01:03:44 +0300

    facade added

---
+++ DesignPatterns/DesignPatternsClasses/Facade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.DesignPatternsClasses
+{
+    /// <summary>
+    /// Name: Facade;
+    /// Other name: Glue;
+    /// Classification: structural, to objects;
+    /// Frequency of use: 5;
+    /// </summary>
+    class Facade
+    {
+        SubSystemA subSystemA = new SubSystemA();
+        SubSystemB subSystemB = new SubSystemB();
+        SubSystemC subSystemC = new SubSystemC();
+
+        public void OperationAB()
+        {
+            subSystemA.OperationA();
+            subSystemB.OperationB();
+            Console.WriteLine("OperationAB was done");
+        }
+        public void OperationBC()
+        {
+            subSystemB.OperationB();
+            subSystemC.OperationC();
+            Console.WriteLine("OperationBC was done");
+        }
+    }
+
+    class SubSystemA
+    {
+        public void OperationA()
+        {
+            Console.WriteLine("Sub System A");
+        }
+    }
+    class SubSystemB
+    {
+        public void OperationB()
+        {
+            Console.WriteLine("Sub System B");
+        }
+    }
+    class SubSystemC
+    {
+        public void OperationC()
+        {
+            Console.WriteLine("Sub System C");
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesignPatterns.DesignPatternsClasses;
 
 namespace DesignPatterns
 {
     class Program
     {
+        // Facade
+        public static void TestFacade()
+        {
+            Console.WriteLine("- {0} -", typeof(Facade).Name);
+            Console.WriteLine();
+
+            Facade facade = new Facade();
+            facade.OperationAB();
+            facade.OperationBC();
+
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            TestFacade();
+
+            Console.ReadLine();
         }
     }
 }
